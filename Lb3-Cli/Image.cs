620e6ae8e16dfb0d58d466b8b76df50f39a68f8a
RuslanUC <dev_ruslan_uc@protonmail.com>
2023-04-26 19:15:42 +0300

    add some methods to Image class

---
+++ Lb3-Cli/Image.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+
+namespace Lb3_Cli { 
+public class Image : Figure {
+    public double Width { get; set; }
+    public double Height { get; set; }
+    public List<Figure> Figures { get; } = new List<Figure>();
+    public string FiguresString {
+        get {
+            string result = "";
+            foreach(Figure figure in Figures) {
+                result += $"{figure}, ";
+            }
+            result = result.Trim();
+            result = result.Remove(result.Length - 1, 1);
+            return result;
+        }
+    }
+
+    public Image() : this(400, 400) {}
+    
+    public Image(double width, double height) {
+        Width = width;
+        Height = height;
+    }
+    
+    public override double GetArea() {
+        return Scaled(Width) * Scaled(Height);
+    }
+
+    public override double GetPerimeter() {
+        return (Scaled(Width) + Scaled(Height)) * 2;
+    }
+    
+    public double GetAreas() {
+        double result = 0;
+        foreach(Figure figure in Figures) {
+            result += figure.GetArea();
+        }
+        return result;
+    }
+
+    public double GetPerimeters() {
+        double result = 0;
+        foreach(Figure figure in Figures) {
+            result += figure.GetPerimeter();
+        }
+        return result;
+    }
+    
+    public void MoveAll(double x = 0, double y = 0) {
+        foreach(Figure figure in Figures) {
+            figure.Move(x, y);
+        }
+    }
+
+    public void MoveAllTo(double x, double y) {
+        foreach(Figure figure in Figures) {
+            figure.MoveTo(x, y);
+        }
+    }
+    
+    public void Merge(Image other) {
+        foreach (Figure figure in other.Figures) {
+            Figures.Add(figure);
+        }
+    }
+
+    public Figure this[int idx] {
+        get {
+            return Figures[idx];
+        }
+    }
+    
+    public override string ToString() {
+        return $"FilledCircle(Location = {Location}, Area = {GetArea()}, Perimeter = {GetPerimeter()}, " +
+               $"Scale = {Scale}, Figures = [{FiguresString}])";
+    }
+}
+}
--- Lb3-Cli/Program.cs
+++ Lb3-Cli/Program.cs
@@ -5,7 +5,10 @@ class Program {
     public static void Main(string[] args) {
         TruncatedCone tc = new TruncatedCone(50, 25, 10);
         tc.Scale = 5;
-        Console.WriteLine(tc);
+        
+        Image i = new Image();
+        i.Figures.Add(tc);
+        Console.WriteLine(i);
     } 
 }
 }
